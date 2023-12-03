f8a5d4abe58345d5c2c719d01fd4c9a62770a24a
Owen.Mather <owenmather7@gmail.com>
2023-12-03 19:12:57 +0100

    Update dir name for each day to pad single digits with 0 for directory sorting
    Fix loop in main.py
    Add force flag to force regenerate

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ def mkdir_if_not_exists(path):
         os.mkdir(path)
 
 
-def generate_aoc_structure(year):
+def generate_aoc_structure(year, force=False):
     mkdir_if_not_exists(year)
-    for i in range(4, 26):
-        mkdir_if_not_exists(os.path.join(year, str(i)))
-        with open(os.path.join(year, str(i), f"{i}.1.py"), 'w') as f:
-            f.write(template.render(year=year, day=i, level=1))
-        with open(os.path.join(year, str(i), f"{i}.2.py"), 'w') as f:
-            f.write(template.render(year=year, day=i, level=2))
+    for day in range(1, 26):
+        day_dir = f'{day:02}'  # Pad single digits with 0 for directory name sorting
+        mkdir_if_not_exists(os.path.join(year, day_dir))
+        for level in [1, 2]:
+            python_file = os.path.join(year, day_dir, f"{day}.{level}.py")
+            if force or not os.path.exists(python_file):
+                with open(python_file, 'w') as f:
+                    f.write(template.render(year=year, day=day, level=level))
 
 
 if __name__ == "__main__":
